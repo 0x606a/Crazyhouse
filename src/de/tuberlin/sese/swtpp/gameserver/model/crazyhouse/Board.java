f73c311d782a6dcc617bb463ecf2b5f48fa8f6b8
AntoniaKnaebel <antonia.knaebel@campus.tu-berlin.de>
2021-02-18 22:21:47 +0100

    board funktionen fertig, muss noch getestet werden

--- src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Board.java
+++ src/de/tuberlin/sese/swtpp/gameserver/model/crazyhouse/Board.java
@@ -1,12 +1,19 @@
 package de.tuberlin.sese.swtpp.gameserver.model.crazyhouse;
 
+import java.util.ArrayList;
+
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import de.tuberlin.sese.swtpp.gameserver.model.crazyhouse.Pferd;
+import de.tuberlin.sese.swtpp.gameserver.model.crazyhouse.Bauer;
+import de.tuberlin.sese.swtpp.gameserver.model.crazyhouse.Dame;
+import de.tuberlin.sese.swtpp.gameserver.model.crazyhouse.Koenig;
+import de.tuberlin.sese.swtpp.gameserver.model.crazyhouse.Laeufer;
+import de.tuberlin.sese.swtpp.gameserver.model.crazyhouse.Turm;
 
-    //converts board string to matrix and a string with spare parts
-//
 public class Board{
     char [][] board;
     String Spare_parts;
@@ -49,29 +56,88 @@ public class Board{
         }
 
     }
-    public void moveBoard(String move) {
-    	if
+    public void moveBoard(String move, String player) throws RuntimeException {
+    	char figure;
+    	char [][] dest = board.clone();
+    	//figur aus spare parts string
+    	if(move.length()==4) {
+    		figure= move.charAt(0);
+    		if(player=="b" & ((int) figure)<97) throw new RuntimeException("not your token");
+    		if(player=="w" & ((int) figure)>90) throw new RuntimeException("not your token");
+    		int dest_x = (((int) move.charAt(2))-97);
+        	int dest_y = (((int) move.charAt(3))-49);
+        	if(dest[dest_y][dest_x]!='0') throw new RuntimeException("invalid move!");
+        	dest [dest_y][dest_x] = figure;
+    		takeSpare_part(figure);
+        	board=dest;
+    		return;
+    		}
+    	
     	//finde figur b3-c4
     	//a=97 h=104
-    	int x = ((int) move.charAt(0))-97;
-    	int y = 7-(((int) move.charAt(1))-49);
-    	char figure = board[y][x];
-    	
+    	else {
+    		int x = ((int) move.charAt(0))-97;
+    		int y = 7-(((int) move.charAt(1))-49);
+    		figure = board[y][x];
+    		if(figure=='0') throw new RuntimeException("no token chosen");
+    		if(player=="b" & ((int) figure)<97) throw new RuntimeException("not your token");
+    		if(player=="w" & ((int) figure)>90) throw new RuntimeException("not your token");
+    		
+    		
+    		ArrayList<String> val_moves= new ArrayList<>();
+    		//qbnrp
     	//figur aufrufen(player übergeben als String)
-    	
-    	
+    		if(figure=='k' || figure=='K') {
+    			Koenig k = new Koenig(player);
+    			val_moves= k.validMoves(board, y, x);
+    		}
+    		if(figure=='q' || figure=='Q') {
+    			Dame d = new Dame(player);
+    			val_moves= d.validMoves(board, y, x);
+    		}
+    		
+    		if(figure=='B' || figure=='b') {
+    			Laeufer l= new Laeufer(player);
+    			val_moves = l.validMoves(board, y, x);
+    		}
+    		if(figure=='n' || figure=='N') {
+    			Pferd p = new Pferd(player);
+    			val_moves = p.validMoves(board, y, x);
+    		}
+    		if(figure=='r' || figure=='R') {
+    			Turm t = new Turm(player);
+    			val_moves = t.validMoves(board, y, x);
+    		}
+    		if(figure=='p' || figure=='P') {
+    			Bauer b = new Bauer(player);
+    			val_moves = b.validMoves(board, y, x);
+    		}
+    		if(!val_moves.contains(move)) throw new RuntimeException("No valid move");
     	//züge vergleichen
-    	int dest_x = (((int) move.charAt(3))-97);
-    	int dest_y = (((int) move.charAt(4))-49);
-    	char [][] dest = board.clone();
-    	if(dest[dest_y][dest_x]!='0') 
-    	dest [dest_y][dest_x] = figure;
-    	dest[y][x]=0;
-    	board = dest;
+    		dest[y][x]=0;
+    		int dest_x = (((int) move.charAt(3))-97);
+    		int dest_y = (((int) move.charAt(4))-49);
     	
+    		if(dest[dest_y][dest_x]!='0') addSpare_part(dest[dest_y][dest_x]); 
+    		dest [dest_y][dest_x] = figure;
+    		board = dest;
+    	}
     	
     }
     public char[][] getBoard(){return board;}
+    private void takeSpare_part(char f) {
+    	int i = 0;
+    	while(i<Spare_parts.length()) {
+    		if(f== Spare_parts.charAt(i)) break;
+    		i++;
+    	}
+    	if(i==Spare_parts.length()) {
+    		throw new RuntimeException("Unvalid move!");
+    	}
+    	String temp = Spare_parts.substring(0, i);
+    	if(i<Spare_parts.length()-1) temp += Spare_parts.substring(i+1);
+    	this.Spare_parts=temp;
+    }
 
     private void addSpare_part(char f) {
     	int val = (int) f;
@@ -81,9 +147,32 @@ public class Board{
     		i++;
     		s= (int) Spare_parts.charAt(i);
     	}
+    	String temp = Spare_parts.substring(0,i) + f + Spare_parts.substring(i);
+    	Spare_parts = temp;
     	
     }
     public String getSpare_parts() {
         return Spare_parts;
+    }
+    public String BoardToString() {
+    	String b_String="";
+    	int cnt=0;
+    	char tmp=9;
+    	for(int y=0; y<8; y++) {
+    		for(int x=0; x<8; x++) {
+    			tmp = board[y][x];
+       			if(tmp=='0') cnt++;
+    			if(tmp!='0' &cnt==0)b_String+=tmp;
+    			if(tmp!='0' &cnt>0) {
+    				b_String += Character.forDigit(cnt,10);
+    				b_String += tmp;
+    				cnt=0;
+    			}
+        		if(x==7 & tmp=='0') b_String += Character.forDigit(cnt,10);
+        	}
+    		b_String+='/';
+    	}
+    	b_String += Spare_parts;
+    	return b_String;	
     }
 }
